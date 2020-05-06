314bfb3e1377629aa906b135c1bf80c777600407
sanderPostma <spostma@sphereon.com>
2020-05-06 14:49:22 +0200

    Temp commit

--- components/FormVerify.vue
+++ components/FormVerify.vue
@@ -84,7 +84,7 @@
   import SForm from '@/components/SForm';
   import SDropFiles from '@/components/SDropFiles';
   import SStatus from '@/components/SStatus';
-  import base64StringToBlob from 'blob-util';
+  import { base64StringToBlob } from 'blob-util';
 
   const base64TestFile = "ewogICJAY29udGV4dCI6IFsKICAgICJodHRwczovL3d3dy53My5vcmcvMjAxOC9jcmVkZW50aWFscy92MSIsCiAgICAiaHR0cHM6Ly93d3cudzMub3JnLzIwMTgvY3JlZGVudGlhbHMvZXhhbXBsZXMvdjEiCiAgXSwKICAiaWQiOiAiaHR0cDovL2V4YW1wbGUuZ292L2NyZWRlbnRpYWxzLzM3MzIiLAogICJ0eXBlIjogWwogICAgIlZlcmlmaWFibGVDcmVkZW50aWFsIiwKICAgICJVbml2ZXJzaXR5RGVncmVlQ3JlZGVudGlhbCIKICBdLAogICJpc3N1ZXIiOiAiZGlkOnYxOnRlc3Q6bnltOno2TWt2U2Jzcm00NFZuaG5nYnlXMnJaazJ1OWJ2U1BVU21Kd3FZak1kNFJTSlQ3QSIsCiAgImlzc3VhbmNlRGF0ZSI6ICIyMDIwLTAzLTEwVDA0OjI0OjEyLjE2NFoiLAogICJjcmVkZW50aWFsU3ViamVjdCI6IHsKICAgICJpZCI6ICJkaWQ6ZmFjdG9tOjVkMGRkNTg3NTcxMTlkZDQzN2M3MGQ5MmI0NGZiZjg2NjI3ZWUyNzVmMGYyMTQ2YzNkOTllNDQxZGEzNDJkOWYiLAogICAgImRlZ3JlZSI6IHsKICAgICAgInR5cGUiOiAiQmFjaGVsb3JEZWdyZWUiLAogICAgICAibmFtZSI6ICJCYWNoZWxvciBvZiBTY2llbmNlIGFuZCBBcnRzIgogICAgfQogIH0sCiAgInByb29mIjogewogICAgInR5cGUiOiAiRWQyNTUxOVNpZ25hdHVyZTIwMTgiLAogICAgImNyZWF0ZWQiOiAiMjAyMC0wNC0yNVQxMToyNToxNVoiLAogICAgImp3cyI6ICJleUpoYkdjaU9pSkZaRVJUUVNJc0ltSTJOQ0k2Wm1Gc2MyVXNJbU55YVhRaU9sc2lZalkwSWwxOS4uVEpYdVhXNWcxb044Y3NsSm1waXZfQUNCLWoyLWN4cXlFRHBKa08zSGZYWWgxb29jTU5raUhvSDYtcDhTR2VWN0VBNlpwc25YV0lMdzB6dGVNUVY3RHciLAogICAgInByb29mUHVycG9zZSI6ICJhc3NlcnRpb25NZXRob2QiLAogICAgInZlcmlmaWNhdGlvbk1ldGhvZCI6ICJkaWQ6djE6dGVzdDpueW06ejZNa3ZTYnNybTQ0Vm5obmdieVcyclprMnU5YnZTUFVTbUp3cVlqTWQ0UlNKVDdBI3o2TWtqRmhSdmJYZmptUThpRkhlWWg0MmNOUzd2NEN0Z3VMenZ3WlNYY0hlOHpxeSIKICB9Cn0K";
 
@@ -95,7 +95,7 @@
 
     data: () => ({
       fields: process.env.NODE_ENV === 'development' ? {
-        files: [ new File([base64StringToBlob(base64TestFile, "application/json") ], `UniversityDegreeCredentialV1.json`) ]
+        files: [ new File([ base64StringToBlob(base64TestFile, "application/json") ], `UniversityDegreeCredentialV1.json`) ]
       } : {
         files: []
       },
